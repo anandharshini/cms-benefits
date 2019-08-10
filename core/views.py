a8b800a783f7cf2c621d4901cb4fb7c06ce43061
anandharshini <anand_dude@hotmail.com>
2019-08-10 15:07:48 -0500

    update s3 get view

--- core/views.py
+++ core/views.py
@@ -94,21 +94,25 @@ def signatureview(request):
     employee = get_employee_instance(request.user, request.GET.get('employee', None))
     heading_message = 'Signature'
     signed_file = ''
+    error = None
     if request.method == 'POST':
         print('Signature', request.POST)
         form = SignatureForm(request.POST)
         if employee:
-            create_pdf_files(employee.id)
-            sign_file = ''.join(['media/submitted/', str(employee.id), '_signature.pdf'])
-            c = canvas.Canvas(sign_file)
-            c.drawImage(request.POST['sign_data'], 10, 10, mask='auto')
-            c.showPage()
-            c.save()
-
-            employee_file = ''.join(['media/submitted/', str(employee.id), '.pdf'])
-            signed_file = ''.join(['media/submitted/', str(employee.id), '_signed.pdf'])
-            signaturemerger(employee_file, sign_file, signed_file)
-            upload_file_to_s3(signed_file, ''.join([str(employee.id), '_signed_pdf']))
+            try:
+                create_pdf_files(employee.id)
+                sign_file = ''.join(['media/submitted/', str(employee.id), '_signature.pdf'])
+                c = canvas.Canvas(sign_file)
+                c.drawImage(request.POST['sign_data'], 10, 10, mask='auto')
+                c.showPage()
+                c.save()
+                
+                employee_file = ''.join(['media/submitted/', str(employee.id), '.pdf'])
+                signed_file = ''.join(['media/submitted/', str(employee.id), '_signed.pdf'])
+                signaturemerger(employee_file, sign_file, signed_file)
+                upload_file_to_s3(signed_file, ''.join([str(employee.id), '_signed_pdf']))
+            except Exception as ex:
+                error = ex
 
     form = SignatureForm()
     # else:
@@ -116,7 +120,8 @@ def signatureview(request):
     return render(request,'healthquestionaire/done.html',
         {
             'form': form, 
-            'signed_pdf_file': ''.join([settings.PREFIX_URL, 'media/submitted/', str(employee.id), '_signed.pdf']) if signed_file != '' else '',
+            'signed_pdf_file': ''.join([settings.PREFIX_URL, 'media/submitted/', str(employee.id), '_signed.pdf']),
             'heading': heading_message,
             'PREFIX_URL': settings.PREFIX_URL,
+            'error_status': error
         })
