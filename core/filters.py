9b2a6835b821f83226cd2adc0103cb423ecde067
anandharshini <anand_dude@hotmail.com>
2019-08-25 15:12:40 -0500

    remove form type

--- core/filters.py
+++ core/filters.py
@@ -3,7 +3,7 @@ from form_application.models import ApplicationModel
 from core.models import LookupModel
 
 class ApplicationModelFilter(django_filters.FilterSet):
-    form_name = django_filters.ModelChoiceFilter(queryset=LookupModel.objects.filter(type_lookup='form_type'))
+    # form_name = django_filters.ModelChoiceFilter(queryset=LookupModel.objects.filter(type_lookup='form_type'))
     class Meta:
         model = ApplicationModel
         fields = ['employee__fk_employer', ]
